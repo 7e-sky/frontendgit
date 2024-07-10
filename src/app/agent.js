8149a33cc5f75b8fa31e52105cc2ca1b84c6c2d7
7e-sky <webmaster@7e-sky.com>
2024-07-10 12:12:32 +0100

    Update agent.js

--- src/app/agent.js
+++ src/app/agent.js
@@ -1,1 +1,12 @@
+import axios from "axios";
+
+// Création d'une instance Axios avec une configuration de base
+const api = axios.create({
+    baseURL: 'https://dev.3findustrie.com/api', // URL de base de votre API Symfony
+    timeout: 5000, // Timeout de 5 secondes pour les requêtes
+    headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${accessToken}`, // Inclure si nécessaire pour les requêtes authentifiées
+    },
+});
 
