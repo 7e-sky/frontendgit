59d7785a1411e7c0412981ead442bba1131ed62d
7e-sky <webmaster@7e-sky.com>
2024-07-11 15:47:11 +0100

    2agent.js

--- src/app/agent.js
+++ src/app/agent.js
@@ -1,12 +1,10 @@
-import axios from "axios";
+import axios from 'axios';
 
-// Création d'une instance Axios avec une configuration de base
-const api = axios.create({
-    baseURL: 'https://dev.3findustrie.com/api', // URL de base de votre API Symfony
-    timeout: 5000, // Timeout de 5 secondes pour les requêtes
+const instance = axios.create({
+    baseURL: 'https://backendgit-qyld3f9nj-7e-skys-projects.vercel.app', // URL de votre backend
     headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${accessToken}`, // Inclure si nécessaire pour les requêtes authentifiées
     },
 });
 
+export default instance;
